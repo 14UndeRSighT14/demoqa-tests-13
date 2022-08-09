7068915b520795b57296cd172d62be2907973807
Янчиев В <yanchievvi@gmail.com>
2022-08-10 00:00:15 +0500

    Убрал неиспользованный импорт

--- src/test/java/tests/RegistrationFormWithParamsTests.java
+++ src/test/java/tests/RegistrationFormWithParamsTests.java
@@ -1,6 +1,5 @@
 package tests;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
